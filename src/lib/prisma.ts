787a41f057f25a81b7daab711c50a3405a09dfc6
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-11-11 23:42:51 +0600

    prisma connection set-up

---
+++ src/lib/prisma.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    // Optional: Prisma connection pool settings
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    // log: ['query', 'info', 'warn', 'error'], // for debugging
+  });
+};
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
